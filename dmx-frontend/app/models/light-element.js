34fda950eb93367514be418da2777c225b4b8af6
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2017-04-16 01:05:26 +0200

    isRgb

--- dmx-frontend/app/models/light-element.js
+++ dmx-frontend/app/models/light-element.js
@@ -11,12 +11,21 @@ export default DS.Model.extend({
 
   isRGB: Ember.computed('channels.@each.name', {
     get() {
-      debugger;
-      const channels = get(this, 'channels').map(c => get(c, 'name'));
-      return channels.length === 3 &&
-        channels.includes('red') &&
-        channels.includes('green') &&
-        channels.includes('blue');
+      let channels = get(this, 'channels').map(c => get(c, 'name'));
+      return Ember.compare(channels.sort(), ['b', 'g', 'r']) === 0;
     }
   }),
+  rgbColor: Ember.computed({
+    get() {
+      const channels = get(this, 'channels');
+      [
+        channels.findBy('name', 'r'),
+        channels.findBy('name', 'r'),
+        channels.findBy('name', 'r'),
+      ].map(c => get(c, 'value')).map(c => c.toString(16));
+    },
+    set(key, val) {
+
+    }
+  })
 });
