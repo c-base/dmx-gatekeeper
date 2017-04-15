000a94711c3e17bc034d317ffbaa966b3e87629d
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2017-04-15 21:41:48 +0200

    deserialize attributes

--- dmx-frontend/app/serializers/light-fixture.js
+++ dmx-frontend/app/serializers/light-fixture.js
@@ -42,5 +42,8 @@ export default ApplicationSerializer.extend({
     });
 
     return relationships;
-  }
+  },
+  keyForAttribute: function(attr) {
+    return Ember.String.underscore(attr);
+  },
 });
