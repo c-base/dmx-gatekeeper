66a448fb7b0559587330c0171edc9b2139bcb3c5
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-07-05 00:06:24 +0200

    white

--- dmx-frontend/app/components/c-base-map/component.js
+++ dmx-frontend/app/components/c-base-map/component.js
@@ -18,6 +18,13 @@ export default Ember.Component.extend({
         .filter(li => get(this, 'selectedIds').includes(get(li, 'id')));
     }
   }),
+  selectWhite: task(function * (on) {
+    const vals = on
+      ? { cw: 102, ww: 51, dim: 128 }
+      : { cw: 0, ww: 0, dim: 0 };
+
+    yield this.get('setVals').perform(vals);
+  }),
   selectColor: task(function * (color) {
     const vals = {
       r: Number.parseInt([...color].slice(1, 3).join(''), 16),
@@ -25,9 +32,12 @@ export default Ember.Component.extend({
       b: Number.parseInt([...color].slice(5, 7).join(''), 16),
     };
 
+    yield this.get('setVals').perform(vals);
+  }),
+  setVals: task(function * (vals) {
     const channels = get(this, 'selectedLightElements')
       .reduce((arr, element) => [...get(element, 'channels').toArray(), ...arr], []);
-    
+
     channels.forEach(c => {
       let name = get(c, 'name');
       if(vals[name] !== undefined) {
