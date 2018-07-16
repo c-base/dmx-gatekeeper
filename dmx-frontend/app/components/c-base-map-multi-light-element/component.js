1451657a47c8eb6b3ea6928d3883629783fc93fc
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-07-16 18:47:12 +0200

    update everything

--- dmx-frontend/app/components/c-base-map-multi-light-element/component.js
+++ dmx-frontend/app/components/c-base-map-multi-light-element/component.js
@@ -2,9 +2,25 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: '',
   selected: computed('selectedIds.[]', 'lightElements.@each.id', {
     get() {
-      return get(this, 'selectedIds').includes(get(this, 'lightElement.id'));
+      return this.lightElements
+        .map(x => x.get('id'))
+        .any(x => this.selectedIds.includes(x))
     }
   }),
+  actions: {
+    select() {
+      const ids = this.lightElements.map(le => le.get('id'));
+
+      if(this.selected) {
+        // unselect
+        this.select(this.selectedIds.filter(x => !ids.includes(x)));
+      } else {
+        // select
+        this.select([...this.selectedIds, ...ids]);
+      }
+    },
+  }
 });
