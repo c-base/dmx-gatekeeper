7bcbf738b66d39a8b44d5a0536e85e8b456b37a6
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-12-01 23:36:20 +0100

    cleanup

--- dmx-frontend/mirage/factories/light-info.js
+++
@@ -1,6 +0,0 @@
-import { Factory, faker } from 'ember-cli-mirage';
-
-export default Factory.extend({
-  posX: () => faker.random.number({ min: 0, max: 800 }),
-  posY: () => faker.random.number({ min: 0, max: 569 }),
-});
